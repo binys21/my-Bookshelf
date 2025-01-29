02d7c50e1a4917fc236722408fd5c707bcddd938
binys21 <binys1121@ewhain.net>
2025-01-29 18:49:10 +0900

    feat: 상세조회에서 이미지 업로드 기능 삭제

--- src/main/java/bookshelf/bookshelf/controller/BookController.java
+++ src/main/java/bookshelf/bookshelf/controller/BookController.java
@@ -2,7 +2,6 @@ package bookshelf.bookshelf.controller;
 
 import bookshelf.bookshelf.dto.BookDto;
 import bookshelf.bookshelf.dto.BookImgDto;
-import bookshelf.bookshelf.service.BookImageService;
 import bookshelf.bookshelf.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.PropertySource;
@@ -13,21 +12,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.io.File;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 
 @Controller
 public class BookController {
     @Autowired
     private BookService bookService;
-
-    @Autowired
-    private BookImageService bookImageService;
     //도서 목록 조회
     @GetMapping("/book/openBookList")
     public ModelAndView openBookList() throws Exception {
@@ -83,45 +75,6 @@ public class BookController {
         return "redirect:/book/openBookList";
     }
 
-    //파일 업로드
-    @PostMapping("/book/uloadImage/{bookId}")
-    public String uploadImage(@PathVariable("bookId")int bookId,
-                              @RequestParam("image") MultipartFile imageFile)  throws Exception {
-        //업로드된 파일 저장할 경로 설정
-        String uploadDirectory="/Users/binys/Desktop/upload";
-        //파일 명으로 파일 경로 설정
-        String fileName=imageFile.getOriginalFilename();
-        String filePath=uploadDirectory+"/"+fileName;
-
-        File destinationFile=new File(filePath);
-        imageFile.transferTo(destinationFile);
-
-        BookImgDto bookImgDto = new BookImgDto();
-        bookImgDto.setImageId(bookId);
-        bookImgDto.setImageUrl(filePath);  // 이미지 URL 저장
-
-        bookImageService.saveBookImage(bookImgDto);
-        return "redirect:/book/openBookDetail/" + bookId;
-    }
-
-    //파일 다운로드 요청 처리
-    @GetMapping("/book/downloadImage/{imageId}")
-    public ResponseEntity<Resource> downloadImage(@PathVariable("imageId") int imageId) throws Exception {
-        //DB에서 이미지 정보 조회
-        BookImgDto bookImgDto = bookImageService.getBookImageById(imageId);
-        String filePath=bookImgDto.getImageUrl();
-
-        Path path = Paths.get(filePath).normalize();
-        Resource resource= new UrlResource(path.toUri());
-
-        if(!resource.exists() || !resource.isReadable()){
-            throw new RuntimeException("파일을 찾을 수 없거나 읽을 수 없습니다."+filePath);
-        }
-        return ResponseEntity.ok()
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
-                .body(resource);
-
-    }
 
 
 
